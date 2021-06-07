e6821e028151289805f4137e2e0aa8432f2b679c
niksergei <75827926+niksergei@users.noreply.github.com>
2021-06-07 11:56:20 +0300

    Add files via upload

---
+++ src/main/java/spring/dao/UserDAOImpl.java
@@ -0,0 +1,63 @@
+package spring.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import spring.model.User;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public List<User> getUserList() {
+        return entityManager.createQuery("select  user from  User  user", User.class).getResultList();
+    }
+
+    @Override
+    @Transactional
+    public User show(int id) {
+        return entityManager.find(User.class,id);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User updatedUser) {
+        entityManager.merge(updatedUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+
+       entityManager.createQuery("delete from User where id = :id").setParameter("id", id).executeUpdate();
+
+
+      //  entityManager.remove(entityManager.find(User.class,id));
+
+    }
+
+    @Override
+    public User getUserByLogin(String name) {
+
+        return entityManager.createQuery("select user from User user where user.name = :name", User.class)
+                .setParameter("name",name).getSingleResult();
+    }
+}
